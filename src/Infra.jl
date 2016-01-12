5d856f0ec5dd30b6d9dff63c4836e9b006a74943
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2016-01-12 04:09:31 -0300

    Using Docker Remote API and minor changes
    * I/O Usage function
    * Added Docker to REQUIRE file
    * TODO: cpu_usage function

--- src/Infra.jl
+++ src/Infra.jl
@@ -1,11 +1,11 @@
 ###=============================================================================
 #
 #          FILE: Infra.jl
-# 
-#         USAGE: include("../Infra.jl") 
-# 
+#
+#         USAGE: include("../Infra.jl")
+#
 #   DESCRIPTION: Julia interface to launch containers through Azure VM
-# 
+#
 #       OPTIONS: ---
 #  DEPENDENCIES: sshpass
 #          BUGS: ---
@@ -17,10 +17,12 @@
 ###=============================================================================
 
 using Requests: get, parse
+using Docker
 
 ###== Top-level variables ======================================================
 
 global host=""
+global url=""
 global passwd=""
 ssh_key=homedir()*"/.ssh/azkey"
 ssh_pubkey=homedir()*"/.ssh/azkey.pub"
@@ -30,10 +32,10 @@ carray_dir= length(LOAD_PATH) == 3 ? LOAD_PATH[3]*"CloudArray/src" : Pkg.dir("Cl
 
 type Container # Abstraction for Docker container
           cid::AbstractString
-          pid::Integer 
+          pid::Integer
           n_of_cpus::Integer
           mem_size::Integer
-      
+
           function Container(cid,pid,n_of_cpus=0,mem_size=512)
               return new(cid,pid,n_of_cpus,mem_size)
           end
@@ -57,13 +59,14 @@ Configures passwordless SSH connections at host `h` whose password is `p`.
 This function calls the `cloud_setup.sh` script which requires `sshpass`.
 
 ```Example
-set_host("cloudarray01.cloudapp.net","password")
+set_host("cloudarray.cloudapp.net","password")
 ```
 """ ->
 function set_host(h::AbstractString,p::AbstractString)
     reply = success(`$(carray_dir)/cloud_setup.sh $h $p`) # set up ssh. if errors occurs, return false
     if (reply)
         global host=h
+        global url=h*":4243"
         global passwd=p
         true
     else
@@ -103,7 +106,7 @@ create_containers(1,2,512)  # 1 container with 2 CPU Cores and 512mb RAM
 ```
 """ ->
 function create_containers(n_of_containers::Integer, n_of_cpus=0, mem_size=512)
-        reserved_mem=200 # reserved memory for initializing a worker into a container 
+        reserved_mem=200 # reserved memory for initializing a worker into a container
         mem_size=mem_size+reserved_mem
         for i in 1:n_of_containers
             ssh_config = false
@@ -111,7 +114,8 @@ function create_containers(n_of_containers::Integer, n_of_cpus=0, mem_size=512)
             port = 3000+get_port()
             # Creating a docker container at VM
             println("Creating container ($key)...")
-            cid = readall(`ssh -i $ssh_key -o StrictHostKeyChecking=no dockeru@$host "docker run -d -p 0.0.0.0:$port:22/tcp --cpuset-cpus="$n_of_cpus" -m=$(mem_size)M cloudarray:latest"`)
+            container = Docker.create_container("$url","cloudarray:latest",memory=mem_size*(10^6),cpuSets="$n_of_cpus",portBindings=[22,"$port"])
+            Docker.start_container("$url",container["Id"])
             # Configuring ssh without password (transfer public key to container)
             println("SSH configuration ($key)... ")
             while !ssh_config
@@ -122,7 +126,7 @@ function create_containers(n_of_containers::Integer, n_of_cpus=0, mem_size=512)
             end
             println("Adding worker ($key)...")
             pid = addprocs(["root@$host"]; tunnel=true,sshflags=`-i $ssh_key -p $port`,dir="/opt/julia/bin",exename="/opt/julia/bin/julia")
-            map_containers[key] = Container(chomp(cid),pid[1],n_of_cpus,mem_size) # Adding Container to Dict
+            map_containers[key] = Container(chomp(container["Id"]),pid[1],n_of_cpus,mem_size) # Adding Container to Dict
         end
 end
 
@@ -138,14 +142,14 @@ create_containers(all)  # delete all containers
 ```
 """ ->
 function delete_containers(args...) #  (splat) variable number of arguments. Ex.: delete_containers(1,2,3) or delete_containers(1:3)
-    containers_rmlist = ""
+    containers_rmlist = Dict()
     if vcat(args...)[1] == all
         for i in collect(keys(map_containers))
             if haskey(map_containers, i) # container exist?
                 container = map_containers[i]
                 rmprocs(container.pid)
                 delete!(map_containers,i)
-                containers_rmlist = containers_rmlist*" $(container.cid)" # adding Counter ID to string
+                Docker.remove_container("$url","$(container.cid)")
             end
         end
     else
@@ -154,12 +158,9 @@ function delete_containers(args...) #  (splat) variable number of arguments. Ex.
                 container = map_containers[i]
                 rmprocs(container.pid)
                 delete!(map_containers,i)
-                containers_rmlist = containers_rmlist*" $(container.cid)" # adding Counter ID to string
+                Docker.remove_container("$url","$(container.cid)")
             end
         end
-    end
-    if (!isempty(containers_rmlist))
-            run(`ssh -i $ssh_key dockeru@$host "docker rm -f $containers_rmlist"`) # deleting containers from Docker
     end
 end
 
@@ -207,15 +208,40 @@ end
 @doc """
 ### mem_usage(key::Integer)
 
-Returns the container memory usage via SSH.
+Returns the container memory usage.
 
 ```Example
 mem_usage(2)
 ```
 """ ->
 function mem_usage(key::Integer)
-    container = map_containers[key]
-    memory = readall(`ssh -i $ssh_key dockeru@$host "cat /sys/fs/cgroup/memory/docker/$(container.cid)/memory.usage_in_bytes"`)
-    parse(Int,memory)/10^6 # convert byte to Megabytes (MB)
-    # returns float
+    Docker.stats_container("$url","$(map_containers[key].cid)")["memory_stats"]["usage"]/10^6
+end
+
+@doc """
+### io_usage(key::Integer)
+
+Returns the number of kilobytes read and written by the cgroup.
+
+```Example
+io_usage(2)
+```
+""" ->
+function io_usage(key::Integer)
+    w = Docker.stats_container("$url","$(map_containers[key].cid)")["blkio_stats"]["io_service_bytes_recursive"][1]["value"]/10^3   # read
+    r = Docker.stats_container("$url","$(map_containers[key].cid)")["blkio_stats"]["io_service_bytes_recursive"][2]["value"]/10^3   # write
+    [w,r]
+end
+
+@doc """
+### io_usage(key::Integer)
+
+Returns the container CPU usage.
+
+```Example
+cpu_usage(2)
+```
+""" ->
+function cpu_usage(key::Integer)
+    #TODO
 end
