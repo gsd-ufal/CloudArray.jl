c46192901a2375ee48546d938c224c868797369a
naelson <naelson17@gmail.com>
2016-03-07 16:55:16 -0300

    uploaded the tests output

--- test/plotting.jl
+++ test/plotting.jl
@@ -1,26 +1,45 @@
-function save_tests()	
 
-	data = readtable("output.csv")
-	
-	step1 = plot_step(data,"Step1")
-	save_svg(step1,"Step1")
 
-	step2 = plot_step(data,"Step2")
-	save_svg(step2,"Step2")
+files = readdir()
 
-	step3fix = plot_step(data,"Step3Fixed")
-	save_svg(step3fix,"Step3Fixed")
+step1= Float64[]
+step2= Float64[]
+step3= Float64[]
 
-	step1rand = plot_step(data,"Step3Random")
-	save_svg(step1rand,"Step3Random")	
+for (f in files)
+	dataset = readtable(f)
+	step1 = [step1,mean(dataset[1])]	
+	step2 = [step2,mean(dataset[2])]	
+	step3 = [step3,mea(dataset[3])]	
 end
 
-function plot_step(data::DataFrame, step::AbstractString)
-	theme = Theme(default_color=colorant"gray",panel_fill=colorant"white")
-	return plot(data,y=step,x=DataFrame(A=[step])[1],Geom.boxplot, theme)
-end
 
-function save_svg(layer::Plot, step::AbstractString)
-	img = SVG(step, 6inch, 4inch)
-	draw(img, layer)
-end
+xlabel("X: Resume size (% of data set)")
+ylabel("Y: Execution time (seconds)")
+grid("on")
+
+
+
+errorbar(x, # Original x data points, N values
+    y, # Original y data points, N values
+    yerr=errs, # Plus/minus error ranges, Nx2 values
+    fmt="o") # Format
+
+
+
+
+errorbar()
+x=[100,50,25,15.5,6.25,3.125,1.5325]
+
+
+errorbar(x, # Original x data points, N values
+    y, # Original y data points, N values
+    yerr=errs, # Plus/minus error ranges, Nx2 values
+    fmt="o") # Format
+
+
+
+
+errorbar(X,step1,yerr=[step1*0.9,y*1.1])
+
+
