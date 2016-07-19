9f480a24d7daa4e81bd2acf09fcb8c6e1feceb42
Naelson Douglas <naelson17@gmail.com>
2016-07-19 06:16:36 -0300

    Implemented the basis for the  process function

--- src/Image_Processing_Service.jl
+++ src/Image_Processing_Service.jl
@@ -11,7 +11,41 @@ end
 function load_time(image_id::Int64)
 end
 
-function process(algorithm, summary_size::Array{Int64,1}, roi::Array{Tuple{Int64,Int64},1}) #roi --> [(x1,y1),(x2,y2)]
+function process(algorithm, summary_size::Tuple{Int64,Int64}, roi::Tuple{Int64,Int64}, start::Tuple{Int64,Int64}) #roi --> [(x1,y1),(x2,y2)]
+	roi=(100,100) #temp
+	summary_size=(10,10) #temp
+	start = (1,1)
+	yRoiLeng = roi[1]
+	xRoiLeng = roi[2]
+	ySummSizeLeng = summary_size[1]
+	xSummSizeLeng = summary_size[2]
+	rowStep = Int64(round(yRoiLeng/ySummSizeLeng))
+	colStep = Int64(round(xRoiLeng/xSummSizeLeng))
+
+	subArray = dataset[start[1]:roi[1]+start[1], start[2]:roi[2]+start[2]]
+
+	if (  (start[1] + roi[1] > length(dataset[:,1])) || 
+		  (start[2] + roi[2] > length(dataset[1,:])) ) 
+		println("Your region of interest overleaps the image size.")
+	end
+
+
+
+
+	buf = ones(10,10) 
+    img = ones(10,10)
+    iterations = 1       
+	f(a) = 10*(a[-1,-1]+ a[-1,+1] + a[-1,0] + a[0,+1]+a[0,-1],a[+1,+1]+a[+1,0],a[+1,-1])
+
+    runStencil(buf, img, iterations, :oob_skip) do b, a
+       b[0,0] = f(a)
+       return a, b
+    end
+
+	
+
+
+
 end
 
 function view(output_id::Int64, format::AbstractString)
