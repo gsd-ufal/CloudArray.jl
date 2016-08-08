728f261b99cb70dfc5721895a46bdacd4b7364a3
naelson <naelson17@gmail.com>
2016-08-08 17:50:46 -0300

    Created a method to select the input image

--- src/Image_Processing_Service.jl
+++ src/Image_Processing_Service.jl
@@ -3,7 +3,24 @@ using ImageView
 include("/home/naelson/repositories/PolSAR.jl/src/ZoomImage.jl")
 include("/home/naelson/repositories/PolSAR.jl/src/PauliDecompositon.jl")
 
-#module ImageProcessingService
+
+#"/home/naelson/Área\ de\ Trabalho/"
+function selectImage(folder, filetype)    
+    
+    files = readdir(imageFolder)
+    filter!(files) do a
+        contains(a,filetype)
+    end
+
+    #an image can have multiple bands in multiple files
+    imagePaths = Array(AbstractString, length(files))
+
+    for (i = 1:length(image))
+        imagePaths[i] = joinpath(imageFolder, files[i])
+    end
+    return imagePaths
+end
+
 function initiate(image_id::Int64, business_model)
 end
 
@@ -25,25 +42,25 @@ end
 function get_bill()
 end
 
-
-
 #Sample algorithm
 function f(a) 
-	(a[-1,-1]+ a[-1,+1] + a[-1,0] + a[0,+1]+a[0,-1]+a[+1,+1]+a[+1,0]+a[+1,-1])
+	((a[-1,-1]+ a[-1,+1] + a[-1,0] + a[0,+1]+a[0,-1]+a[+1,+1]+a[+1,0]+a[+1,-1])*0.8)
 end
 
 
-src_height= 2500
-src_width = 2000
+src_height= 11858
+src_width = 1650
 roiHeight= 2500
 roiWidth = 1600
 zoomHeight  = 1000
 zoomWidth   = 1000
+startPos = (1,1)
 src = open("SanAnd_05508_10007_005_100114_L090HHHH_CX_01.mlc")
 
 
 function areLimitsWrong(summary_height,src_height,summary_width,src_width,starting_line,roi_height,roi_width,starting_col)
 #checking if the summary overleaps the roi
+print("\n")
 print("summary_height: ",summary_height,"\n")
 print("src_height: ",src_height,"\n")
 print("src_width: ",src_width,"\n\n")
@@ -63,20 +80,15 @@ print("roi_height: ",roi_height,"\n")
 		println("Your ROI height overleaps the source height")
 		return true
 	end
-		#Checking if roi_x > src_x
-		300 + 1600 > 2000
+		#Checking if roi_x > src_x		
 	if (starting_col-1 + roi_width > src_height)
 		println("Your ROI width overleaps the source frame.")
 		return true
 	end
-
 	return false
 end
 
-
-
-function process(algorithm=f, summary_size::Tuple{Int64,Int64}=(zoomWidth,zoomHeight), roi::Tuple{Int64,Int64}=(roiHeight-1,roiWidth-1), start::Tuple{Int64,Int64} = (1,1); debug::Bool=false, img::IOStream=src) 
-
+function process(algorithm, summary_size::Tuple{Int64,Int64}, roi::Tuple{Int64,Int64}, start::Tuple{Int64,Int64}; debug::Bool=false, img::IOStream=src) 
 	starting_line = start[1]
 	starting_col = start[2]	
 	starting_pos =  starting_line + (starting_col-1)*src_width
@@ -90,46 +102,29 @@ function process(algorithm=f, summary_size::Tuple{Int64,Int64}=(zoomWidth,zoomHe
 	row_step = Int64(round(roi_height/summary_height))
 	col_step = Int64(round(roi_width/summary_width))	
 
-	#TODO put these checks in one function
-	
 
+	if (areLimitsWrong(summary_height,src_height,summary_width,src_width,starting_line,roi_height,roi_width,starting_col))		
+	else 		
+		roi_subarray = ZoomImage(starting_pos, roi_height, roi_width, summary_height, summary_width, src_height, src_width, src) 
 
+		println("Deu zoom suave")		
+		println("Deu reshape suave")
 
-if (areLimitsWrong(summary_height,src_height,summary_width,src_width,starting_line,roi_height,roi_width,starting_col))
-	#The function will print a warning and the program will stop
-else 
-	##TODO: Fix the ZoomImage function to make it return a matrix instead of an array
-	roi_subarray = ZoomImage(starting_pos, roi_height, roi_width, summary_height, summary_width, src_height, src_width, src) 
+		roi_subarray = PauliDecomposition(roi_subarray, roi_subarray, roi_subarray, summary_height, summary_width)			
 
+		buffer = Array(Real,summary_height,summary_width) 
 
-	println("Deu zoom suave")
+		iterations = 4
+		println("Criou buffer suave")
+		runStencil(buffer, roi_subarray, iterations, :oob_src_zero) do b, a
+			b[0,0] =  algorithm(a)
+			return a, b
+		end
 
-	#roi_subarray = reshape(roi_subarray, summary_height,summary_width)
-	println("Deu reshape suave")
-
-	roi_subarray = PauliDecomposition(roi_subarray, roi_subarray, roi_subarray, summary_height, summary_width)
-	
-	
-
-	buffer = Array(Real,summary_height,summary_width) 
-
-
-	iterations = 1
-	println("Criou buffer suave")
-	runStencil(buffer, roi_subarray, iterations, :oob_src_zero) do b, a
-		b[0,0] =  algorithm(a)
-		return a, b
+		return buffer
 	end
-
-
-	return roi_subarray
 end
 
-
-
-
+function process()
+	return process(f, (zoomWidth,zoomHeight), (roiHeight-1,roiWidth-1), startPos) 
 end
-
-
-
-#end
